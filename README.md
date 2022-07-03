883dbca56c2760af34222f41ea749ad876a8abf2
Adilson Pina <adilson.gditecnologia@gmail.com>
2022-07-02 23:58:13 -0300

    ajustes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ php artisan make:controller ProdutosController --resource
 
 php artisan make:model Produto
 
+
+composer require laravel/passport
+php artisan migrate
+php artisan passport:install
--- database/migrations/2022_05_27_032234_produto.php
+++ database/migrations/2022_05_27_032234_produto.php
@@ -16,8 +16,17 @@ return new class extends Migration
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('price');
+            $table->decimal('price', 8, 2);
+            $table->string('img');
+            $table->unsignedBigInteger('id_categoria');
+            $table->unsignedBigInteger('id_marca');
             $table->timestamps();
+
+            /**
+             * Foreign Keys
+             */
+            $table->foreign('id_categoria')->references('id')->on('categorias');
+            $table->foreign('id_marca')->references('id')->on('marcas');
         });
     }
 
