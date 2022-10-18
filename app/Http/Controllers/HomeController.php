4a73ce9848514f715ce3fc5f1b9189b611fb5e65
Adilson Pina <adilson.gditecnologia@gmail.com>
2022-10-18 17:27:15 -0300

    ajuste

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OauthAccessTokens;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,23 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+       // $this->middleware('auth');
+     
+
+       
+    }
+
+
+    public function show(Request $request)
+    {
+        return OauthAccessTokens::find($request->get("token"));
+    }
+
+    public function find($id)
+    {
+        $this->user = $this->user::find($id);
+
+        return $this->user;
     }
 
     /**
